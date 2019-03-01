bc4b9b6f8983b7f8184384c4183ab42fc2c5b36c
chwech <804699297@qq.com>
2019-03-01 11:08:48 +0800

    。

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,12 @@
-/// <reference path="utils.d.ts" />
-/// <reference path="cookie.d.ts" />
+export function noop(): void;
+export function transferUrl(url: string, params: array): string;
+export function serializeQueryStr(params: object, sep = '&'): string;
+export function getQueryString(name: string): string | null;
+
+export class Cookie {
+  constructor();
+  get(name: string): string;
+  set(name: string, value: string, expires?: string, path?: string, domain?: string, secure?: string): void;
+  unset(name, path?: string, domain?: string, secure?: string): void;
+}
 
