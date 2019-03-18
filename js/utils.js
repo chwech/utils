f6030cc422ff175f6ba66d109d9f83c1d10ff7e1
chwech <804699297@qq.com>
2019-03-18 17:17:08 +0800

    添加toType和filterNull函数

--- js/utils.js
+++ js/utils.js
@@ -48,3 +48,25 @@ export function getQueryString(name) {
   if (r != null) return unescape(r[2]);
   return null;
 }
+
+// 自定义判断元素类型JS
+export function toType(obj) {
+  return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase()
+}
+
+// 参数过滤函数
+export function filterNull(o) {
+  for (var key in o) {
+    if (o[key] === null) {
+      delete o[key]
+    }
+    if (toType(o[key]) === 'string') {
+      o[key] = o[key].trim()
+    } else if (toType(o[key]) === 'object') {
+      o[key] = filterNull(o[key])
+    } else if (toType(o[key]) === 'array') {
+      o[key] = filterNull(o[key])
+    }
+  }
+  return o
+}
