7fae89446d7793a5215487b5edb56711d25aa879
陈伟成 <804699297@qq.com>
2020-04-26 17:25:45 +0800

    test: 拼接授权url函数添加测试

--- src/wx.test.js
+++ src/wx.test.js
@@ -2,11 +2,18 @@ import { joinScopeUrl } from './wx.ts'
 
 it('正确拼接微信授权url', () => {
   const result = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf0e81c3bee622d60&redirect_uri=http%3A%2F%2Fnba.bluewebgame.com%2Foauth_response.php&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect'
-  const res = joinScopeUrl('wxf0e81c3bee622d60', 'http://nba.bluewebgame.com/oauth_response.php', 'STATE')
+  const res = joinScopeUrl('wxf0e81c3bee622d60', 'http://nba.bluewebgame.com/oauth_response.php', undefined, 'STATE')
   expect(res).toBe(result)
 
   const result1 = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base&state=123#wechat_redirect'
-  const res1 = joinScopeUrl('wx520c15f417810387', 'https://chong.qq.com/php/index.php?d=&c=wxAdapter&m=mobileDeal&showwxpaytitle=1&vb2ctag=4_2030_5_1194_60', '123', 'snsapi_base')
+  const res1 = joinScopeUrl('wx520c15f417810387', 'https://chong.qq.com/php/index.php?d=&c=wxAdapter&m=mobileDeal&showwxpaytitle=1&vb2ctag=4_2030_5_1194_60', 'snsapi_base', '123')
   expect(res1).toBe(result1)
 
+  const result2 = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base#wechat_redirect'
+  const res2 = joinScopeUrl('wx520c15f417810387', 'https://chong.qq.com/php/index.php?d=&c=wxAdapter&m=mobileDeal&showwxpaytitle=1&vb2ctag=4_2030_5_1194_60', 'snsapi_base')
+  expect(res2).toBe(result2)
+
+  const result3 = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_userinfo#wechat_redirect'
+  const res3 = joinScopeUrl('wx520c15f417810387', 'https://chong.qq.com/php/index.php?d=&c=wxAdapter&m=mobileDeal&showwxpaytitle=1&vb2ctag=4_2030_5_1194_60')
+  expect(res3).toBe(result3)
 })
--- src/wx.ts
+++ src/wx.ts
@@ -1,13 +1,13 @@
 
-export function joinScopeUrl (appId: string, redirectUri: string, state: string, scope = 'snsapi_userinfo'): string {
+export function joinScopeUrl (appId: string, redirectUri: string, scope = 'snsapi_userinfo', state?: string,): string {
   let url = `https://open.weixin.qq.com/connect/oauth2/authorize?`
   url += `appid=${appId}&`
   url += `redirect_uri=${encodeURIComponent(redirectUri)}&`
   url += `response_type=code&`
-  url += `scope=${scope}&`
+  url += `scope=${scope}`
 
   if (state) {
-    url += `state=${state}`
+    url += `&state=${state}`
   }
 
   url += `#wechat_redirect`
